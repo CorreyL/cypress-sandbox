de79141fc509482b91fe4b71253e49ba05d6eadb
CorreyL <correylim@gmail.com>
2024-06-20 16:25:01 -0700

    Define const vars to build the base uniqlo URL

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,9 @@
+const BASE_URL = 'https://www.uniqlo.com/';
+const region = 'ca';
+const language = 'en';
+
+const websitePath = `${BASE_URL}${region}/${language}`;
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
