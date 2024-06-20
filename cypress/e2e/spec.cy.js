02d7ed6408ca59050c0fa7251908ea54f9925b2f
CorreyL <correylim@gmail.com>
2024-06-20 16:25:30 -0700

    Add method to build a search query URL

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,6 +4,17 @@ const language = 'en';
 
 const websitePath = `${BASE_URL}${region}/${language}`;
 
+/**
+ * Builds the full URL for performing a search of the provided query on the
+ * Uniqlo website
+ *
+ * @param {string} searchQuery The user's search query string
+ * @returns {string} The URL to perform the search query
+ */
+const searchUrl = (searchQuery) => {
+  return `${websitePath}/search/?q=${searchQuery}`;
+};
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
