be6ebe98299d759793e547384ce8e5eb64eaaab6
CorreyL <correylim@gmail.com>
2024-06-20 17:13:01 -0700

    Add test to retrieve & assert # of search results

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -21,7 +21,19 @@ describe('search for sweatpants', () => {
     await cy.wait(3000);
   });
 
-  it('passes', () => {
-
+  it('retrieves the number of search results', async () => {
+    cy.get('.fr-results-count > .fr-form-label').should((result) => {
+      // There should only be one DOM element that matches the above selector
+      expect(result).to.have.length(1);
+      // Make sure the textContent is not empty
+      expect(result[0].textContent).to.not.equal('');
+      // When split on the space character, there should be two items in the array
+      expect(result[0].textContent.split(' ')).to.have.length(2);
+      // Check that a number was retrieved
+      const numberOfSearchResults = result[0].textContent.split(' ')[0];
+      expect(Number.isNaN(numberOfSearchResults)).to.be.false;
+      // There should have been at least one result
+      expect(Number.parseInt(numberOfSearchResults)).to.be.greaterThan(0);
+    });
   })
 })
